5bc667ce0112cf139dbd47d9368b19d8b438f66b
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2022-10-09 13:31:58 +0000

    adding source methid in main

--- scripts/ado_elastic_pool.sh
+++ scripts/ado_elastic_pool.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -eu
+set -e
 
 main () {
   printf "mode: %s\n" "$mode"
@@ -11,4 +11,12 @@ main () {
 # shellcheck disable=SC1091
 source ./scripts/functions.sh
 mode="$1"
-main
+
+# similar method in bash to the Python:
+# if __name__ == "__main__":
+#     main()
+# to allow the distinction of being called from the command line or via source
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
+then
+  main
+fi
